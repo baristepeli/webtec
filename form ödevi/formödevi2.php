0c84c4664e87886376f364f00634b9f4713e18b2
baristepeli <101328759+baristepeli@users.noreply.github.com>
2022-05-18 13:13:41 +0300

    Add files via upload

---
+++ form ödevi/formödevi2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Ödevi 2</title>
+</head>
+<body>
+    
+<form action="/action_page.php">
+  <label for="fname">Ad Soyad:</label><br>
+  <input type="text" id="fname" name="fname" ><br>
+  <label for="mail">Email:</label><br>
+  <input type="text" id="mail" name="email" ><br>
+  <label for="tel">Telefon:</label><br>
+  <input type="text" id="tel" name="phone" ><br>
+  <label for="male">Cinsiyet:</label><br>
+  <input type="radio" id="male" name="male" value="">
+  <label for="male">Erkek</label><br>
+<input type="radio" id="male" name="male" value="">
+  <label for="male">Kadın</label><br>
+<label for="lessons">Ders:</label><br>
+<input type="radio" id="lessons1" name="lessons1" value="">
+  <label for="lessons1">Yazılım Mimarileri</label><br>
+<input type="radio" id="lessons2" name="lessons2" value="">
+  <label for="lessons2">Web Programlama</label><br>
+<input type="radio" id="lessons3" name="lessons3" value="">
+  <label for="lessons3">Sunucu İşletim Sistemleri</label><br>
+  <label for="note">Not:</label><br>
+  <input type="score" id="score" name="score" ><br><br>
+  <input type="submit" value="Submit">
+
+</form> 
+</body>
+</html>
